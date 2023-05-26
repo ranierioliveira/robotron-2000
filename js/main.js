9d6a8f676b37d6c6cf8fe4e187c1e5a02f4953d7
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-26 15:12:45 -0300

    Alterando vários componentes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 const subtrair = document.querySelector('#subtrair');
 const somar = document.querySelector('#somar');
 const braco = document.querySelector('#braco');
+const controle = document.querySelectorAll('.controle-ajuste');
 
+controle.forEach((elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent);
+        console.log(evento)
+    })
+})
 
-somar.addEventListener('click', () => {
-    braco.value = Number(braco.value) + 1;
-});
-
-subtrair.addEventListener('click', () => {
-    braco.value = Number(braco.value) - 1;
-});
+function manipulaDados(operacao){
+    if(operacao === '-'){
+        braco.value = Number(braco.value) - 1;
+    } else {
+        braco.value = Number(braco.value) + 1;
+    }
+}
